84592ff43d9593317e0fb1f0cca2bfc682788e51
Joshua Yun <joshuayun1999@gmail.com>
2019-02-21 12:44:42 -0800

    Finished the Assignment

--- a.cpp
+++ a.cpp
@@ -13,11 +13,12 @@ struct PERSON{
 //Function names
 void Display(PERSON *p, int a);
 void FindRichest(PERSON *p, int a);
-void Deposit(char C[],PERSON *p, int a);
+void Deposit(char CustName[],PERSON *p, int a);
 void NewCopy(string firstname, PERSON *p, int a);
 
 int main()
 {
+    string name;
     int a=0;
     ifstream in;
     in.open("data.txt");
@@ -30,16 +31,19 @@ int main()
     PERSON *p = new PERSON[a];
     string firstname, lastname;
     int x=0;
-    while(in>>firstname>>lastname>>p[x].Balance){
+    while(in>>firstname>>lastname>>p[x].Balance)
+    {
         strcpy(p[x].Name, (firstname+" "+lastname).c_str());
         x++;
     }
     Display(p, a);
     FindRichest(p, a);
-    char C[20];
+    char CustName[20];
     cout<<"Enter your full name to deposit money:";
-    cin.getline(C, 20);
-    Deposit(C, p, a);
+    getline(cin,name);
+    strcpy(CustName, name.c_str());
+    // cin.getline(C, 20);
+    Deposit(CustName, p, a);
     NewCopy("data.txt", p, a);
     return 0;
 }
@@ -62,12 +66,34 @@ void FindRichest(PERSON *p, int N){
     cout<<"The customer with maximum balance is "<<p[max].Name<<endl;
 }
 
-void Deposit(char C[],PERSON *p, int N)
+void Deposit(char CustName[],PERSON *p, int N)
 {
-
+  int index = 0;
+  for (int i = 0; i < N; i++)
+  {
+    if(strcmp(CustName, p[i].Name)==0)
+    {
+        index = i;
+        break;
+    }
+  }
+  float deposit;
+  cout << endl;
+  cout << CustName << ", how much would you like to deposit? ";
+  cout<<"\nNow your new balance is ";
+  cin>>deposit;
+  p[index].Balance+=deposit;
+  cout<<"New balance is $"<<p[index].Balance<<endl;
 }
 
 void NewCopy(string firstname, PERSON *p, int N)
 {
-
+  ofstream out;
+  out<<setprecision(2)<<fixed;
+  out.open(firstname.c_str());
+  for(int i=0; i<N; i++)
+  {
+      out<<p[i].Name<<" "<<p[i].Balance<<endl;
+  }
+  out.close();
 }
